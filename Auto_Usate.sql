6f7ba8593583e8ce009f22fa3028da0212b95e01
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-16 16:23:11 +0100

    add neopatentati row

--- Auto_Usate.sql
+++ Auto_Usate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 16, 2023 at 03:16 PM
+-- Generation Time: Jan 16, 2023 at 03:22 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.0
 
@@ -37,6 +37,7 @@ CREATE TABLE `Auto Usate` (
   `Anno` year(4) NOT NULL,
   `Immatricolazione` date DEFAULT NULL,
   `Proprietario` varchar(200) DEFAULT NULL,
+  `Neopatentati` tinyint(1) NOT NULL,
   `Targa` char(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -60,7 +61,7 @@ ALTER TABLE `Auto Usate`
 -- AUTO_INCREMENT for table `Auto Usate`
 --
 ALTER TABLE `Auto Usate`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
